411dc11367a07ec4d9ab9287b6f8da55d019acdb
Konrad <konrad@devinside.de>
2024-07-24 12:36:52 +0300

    Rewrites for shortcodes and working on home-section

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,20 @@ const eleventySass = require("eleventy-sass");
 
 module.exports = function (eleventyConfig) {
 
-	// Thumbnail-Plugin
-	eleventyConfig.addShortcode("image", async function (src, alt, sizes) {
+	/**
+	 * shortcode.thumbs
+	 * Adds the thumbnail-plugin as 11ty-shortcode. It resizes the given image according to the sizes defined in the thmbs array
+	 * 
+	 * @param {string} src	 		path of the image
+	 * @param {string} alt 			alt text of the image
+	 * @param {string} sizes 		srcset-sizes
+	 * @param {Array} thmbs 		thmbs-sizes. If not set, it will rely on the default sizes of 300,600,1000
+	 * 
+	 * @returns {string} 			Renders a picture-element with the thmbs-srcset as webp and jpeg as fallback
+	 */
+	eleventyConfig.addShortcode("thumbs", async function (src, alt, sizes, thmbs=[300, 600, 1000]) {
 		let metadata = await Image(src, {
-			widths: [300, 600, 1000],
+			widths: thmbs,
 			formats: ["webp", "jpeg"],
 			outputDir: "_public/img"
 		});
@@ -20,6 +30,20 @@ module.exports = function (eleventyConfig) {
 
 		return Image.generateHTML(metadata, imageAttributes);
 	});
+
+
+	/**
+	 * shortcode.img
+	 * Renders the html img-tag
+	 *
+	 * @param   {string}  src       img-src
+	 * @param   {string}  title     title for alt/title-tag
+	 *
+	 * @return  {string}            Simple HTML <img>-Tag
+	 */
+	eleventyConfig.addShortcode("img", function(src,title){
+		return `<img src="img/${src}" title="${title}" alt="${title}" />`
+	})
 
 	// SASS-Plugin
 	eleventyConfig.addPlugin(eleventySass, {
@@ -35,9 +59,11 @@ module.exports = function (eleventyConfig) {
 		rev: false
 	});
 
+
 	// Copy img-folder including content
 	eleventyConfig.addPassthroughCopy("img");
 
+	
 	// Set directories
 	return {
 		dir: {
